fa9692c9978e1c9e7d096520433d6a5b9b188f38
Lenci Cruz <kristellenci@gmail.com>
2023-12-01 22:41:36 +0800

    v minor tweaks

--- lib/utils/game2_provider.dart
+++ lib/utils/game2_provider.dart
@@ -9,7 +9,7 @@ class NumberdleGame extends HigherLowerGame {
     HigherLowerGame.game_guess = "${digits[0]}${digits[1]}${digits[2]}${digits[3]}";
   }
 
-  //checking world
+  //checking word
   bool noRepeating(String input) {
     for (int i = 0; i < input.length - 1; i++) {
       for (int j = i + 1; j < input.length; j++) {
